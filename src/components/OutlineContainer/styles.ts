9c9e106a7fab1b0ec60bdf2ed06985f3b141949a
Osvaldo <osvaldobbini@gmail.com>
2024-08-26 20:55:42 -0300

    change size

--- src/components/OutlineContainer/styles.ts
+++ src/components/OutlineContainer/styles.ts
@@ -14,6 +14,7 @@ export const Container = styled(motion.div)`
   text-decoration: none;
   background: #ffff;
   text-align: center;
+  font-size: 12px;
   color: ${({ theme }) => theme.colors.primary.dark};
   border-radius: 10px;
   box-shadow: 10px 10px rgba(0,0,0,0.75);
